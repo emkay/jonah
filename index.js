c5cfe70fe6881c8d1794b43f1847861d955a3c8e
Michael Matuzak <mmatuzak@gmail.com>
2012-09-15 21:17:12 -0700

    boom!

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports.ascii = ascii = function (hex) {
+    return hex.match(/.{2}/g).map(function (n) {
+        return String.fromCharCode(parseInt(n, 16));
+    }).join('');
+}
+
+module.exports.hex = hex = function (s) {
+    return s.split('').map(function (c) {
+        return c.charCodeAt(0).toString(16);
+    }).join('');
+}
